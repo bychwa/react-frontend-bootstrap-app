60b4e5eff9de7efb10762e8a6784c78a7a8e7331
jacksonisack <jacksonisack@gmail.com>
2018-02-25 22:49:08 +0100

    added image

--- app/components/app/app.view.tsx
+++ app/components/app/app.view.tsx
@@ -8,21 +8,33 @@ type AppViewProps = {
 };
 
 export const AppView = (props: AppViewProps) => {
+  const thePanther = require('../../assets/thepanther.png');
+  const thePantherWhite = require('../../assets/thepanther-white.png');
   return (
     <Container>
       <br />
       <Header size='huge' content='START HERE!' textAlign='center' style={{ color: 'white' }} />
-      <Card fluid>
-        <Card.Content extra>
-          <Segment color={ props.app.isPinging ? 'red' : 'blue' }>
+      <Card fluid={true}>
+        <Card.Content textAlign='center'>
+          <img
+            src={ props.app.isPinging ? thePantherWhite : thePanther }
+            width='30%'
+          />
+        </Card.Content>
+        <Card.Content extra={true}>
+          <Segment color={props.app.isPinging ? 'red' : 'blue'} raised={true}>
             <Header
               size='huge' textAlign='center'
-              content={ props.app.isPinging ? 'PINGING...' : 'PONG!' }
+              content={props.app.isPinging ? 'PINGING...' : 'PONG!'}
             />
           </Segment>
         </Card.Content>
         <Card.Content textAlign='center'>
-          <Button primary onClick={ props.app.isPinging ? props.onPong : props.onPing } content='PING ME' />
+          <Button
+            primary={true}
+            onClick={props.app.isPinging ? props.onPong : props.onPing}
+            content={props.app.isPinging ? 'PONG!' : 'PING ME'}
+          />
         </Card.Content>
       </Card>
     </Container>);
